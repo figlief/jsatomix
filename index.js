427c43885c281c9bca3615604d9a38cd1908752f
figlief <figlief@figlief.com>
2016-04-25 06:50:45 +0100

    fix index.js to make it a proper package

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var l = require('jsatomix-levelsets')
-var _ = require('lodash')
-
-_.forOwn(l, function (v, k) {
-  console.log(k, v.levels.length)
-})
+module.exports = {
+  'levelsets': require('jsatomix-levelsets'),
+  'imagenames': require('./lib/imagenames.js')
+}
