8338df4ce16a6590206a331acb2e2c63982d90bc
figlief <figlief@figlief.com>
2016-04-25 04:49:07 +0100

    Add imagenames module.

---
+++ test/imagenames.js
@@ -0,0 +1,14 @@
+var test = require('tape')
+var names = require('../lib/imageref.js')
+
+test('litmus', function (t) {
+  t.plan(2)
+  t.looseEqual(
+    names(['1', '']),
+    ['atom-h']
+  )
+  t.looseEqual(
+    names(['1', 'gc']),
+    ['bond-left', 'bond-right', 'atom-h']
+  )
+})
